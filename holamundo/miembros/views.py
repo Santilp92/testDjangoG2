995b5787d4167bebf9c0c72dd1c7aefe6471dbe6
Andrés Sánchez <diaverde77@gmail.com>
2022-09-06 21:41:44 -0500

    Rutas GET y POST

--- holamundo/miembros/views.py
+++ holamundo/miembros/views.py
@@ -1,5 +1,9 @@
+import json
+from unicodedata import name
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotAllowed
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest
+
+from .models import Members
 
 def index(request):
     return HttpResponse("Hola, mundo")
@@ -7,12 +11,34 @@ def index(request):
 def newMembers(request):
     #print(request.method)
     if request.method == 'POST':
-        return HttpResponse("Va a agregar un nuevo miembro")
+        try:
+            data = json.loads(request.body)
+            #print(data)
+            #print(type(data))
+            member = Members(
+                name = data["nombre"],
+                email = data["correo"]
+            )
+            #print(member)
+            member.save()
+            return HttpResponse("Nuevo miembro agregado")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
     else:
         return HttpResponseNotAllowed(['POST'], "Método inválido")
 
 def getMembers(request):
     if request.method == 'GET':
-        return HttpResponse("Esta es la información de los miembros")
+        members = Members.objects.all()
+        allMembersData = []
+        for x in members:
+            data = {"documento": x.id, "nombre": x.name, "email": x.email}
+            allMembersData.append(data)
+        dataJson = json.dumps(allMembersData)
+        #print(dataJson)
+        resp = HttpResponse()
+        resp.headers['Content-Type'] = "text/json"
+        resp.content = dataJson
+        return resp
     else:
         return HttpResponseNotAllowed(['GET'], "Método inválido")
